398d19203e0f7b0724e906e7098fe5c91653e4f0
alrx31 <usov.lesha2017@gmail.com>
2025-09-12 16:12:47 +0300

    fix font color

--- LT1/main.cpp
+++ LT1/main.cpp
@@ -29,19 +29,25 @@ int main(int argc, char** argv) {
         std::cerr << "Usage: " << argv[0] << " <m rows> <n cols> [frame size] [bgcolor hex] [linecolor hex] [line size]\n";
         return 1;
     }
-
     
     int m = std::stoi(argv[1]);
     int n = std::stoi(argv[2]);
-    int frameSize = (argc >= 4) ? std::stoi(argv[3]) : 1;
+    int frameSize = (argc >= 4) ? std::stoi(argv[3]) : 10;
+    
+    sf::Color borderColor = sf::Color::Red;
+    sf::Color textColor = sf::Color::Red;
+    
+    ColorParse::hexToColor("#0000FF", borderColor);
+    ColorParse::hexToColor("#FF0000", textColor);
     
     sf::Color bgColor = sf::Color::Black;
     sf::Color lineColor = sf::Color::White;
     
     if (argc >= 5) ColorParse::hexToColor(argv[4], bgColor);
     if (argc >= 6) ColorParse::hexToColor(argv[5], lineColor);
-
+    
     float lineSize = argc >= 7 ? std::stof(argv[6]) : 1.f;
+    int fontSize = 2.0f;
 
     auto desktop = sf::VideoMode::getDesktopMode();
     unsigned int width = desktop.size.x;
@@ -74,11 +80,12 @@ int main(int argc, char** argv) {
         float targetW = cellWidth * 0.8f;
         unsigned int size = 40;
 
-        while (size > 6 && bounds.size.x > targetW) {
-            size -= 2;
-            t.setCharacterSize(size);
-            bounds = t.getLocalBounds();
-        }
+        //while (size > 6 && bounds.size.x > targetW) {
+        //    size -= 2;  
+        //}
+
+        t.setCharacterSize(size*fontSize);
+        bounds = t.getLocalBounds();
 
         float cx = frameSize + j * cellWidth + cellWidth / 2.f;
         float cy = frameSize + cellHeight / 2.f;
@@ -90,7 +97,6 @@ int main(int argc, char** argv) {
 
         headers.push_back(t);
     }
-
 
     while (window.isOpen()) {
         while (auto eventOpt = window.pollEvent()) {
@@ -109,16 +115,36 @@ int main(int argc, char** argv) {
 
         window.clear(bgColor);
 
-        for (int i = 0; i <= m; i++) {
-            sf::RectangleShape line({(float)width - frameSize * 2, lineSize});
+        sf::RectangleShape topBorder(sf::Vector2f(width, (float)frameSize));
+        topBorder.setPosition(sf::Vector2f(0, 0));
+        topBorder.setFillColor(borderColor);
+        window.draw(topBorder);
+
+        sf::RectangleShape bottomBorder(sf::Vector2f(width, (float)frameSize));
+        bottomBorder.setPosition(sf::Vector2f(0, (float)height - frameSize));
+        bottomBorder.setFillColor(borderColor);
+        window.draw(bottomBorder);
+        
+        sf::RectangleShape leftBorder(sf::Vector2f((float)frameSize, height));
+        leftBorder.setPosition(sf::Vector2f(0, (float)frameSize));
+        leftBorder.setFillColor(borderColor);
+        window.draw(leftBorder);
+        
+        sf::RectangleShape rightBorder(sf::Vector2f((float)frameSize, height));
+        rightBorder.setPosition(sf::Vector2f(width - frameSize, (float)frameSize));
+        rightBorder.setFillColor(borderColor);
+        window.draw(rightBorder);
+
+        for (int i = 1; i < m; i++) {
+            sf::RectangleShape line({(float)width - frameSize * 2, (float)lineSize});
             line.setFillColor(lineColor);
             float y = frameSize + i * cellHeight;
             line.setPosition(sf::Vector2f(frameSize,y));
             window.draw(line);
         }
 
-        for (int j = 0; j <= n; j++) {
-            sf::RectangleShape vline({lineSize, (float)height - frameSize * 2});
+        for (int j = 1; j < n; j++) {
+            sf::RectangleShape vline({(float)lineSize, (float)height - frameSize * 2});
             vline.setFillColor(lineColor);
             float x = frameSize + j * cellWidth;
             vline.setPosition(sf::Vector2f(x, frameSize));
